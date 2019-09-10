5d9101da964ad02443b54e689d462d37f459924b
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-10 19:14:35 -0400

    Added SignUp Component...
    implemented firebase utils and added abillity to store authenticated
    users to firestore database.

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.components.jsx';
+import SignUp from '../../components/sign-up/sign-up.component.jsx';
 
 import './sing-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
-)
+);
 
 export default SignInAndSignUpPage;
