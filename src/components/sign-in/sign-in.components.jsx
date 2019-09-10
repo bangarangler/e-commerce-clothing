7ac02005fcde387caefd4a6c9335323486f242bd
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-10 10:23:37 -0400

    Tweaked Custom button to accept prop...
    prop set stylings for sing in with google button
    fixed box-sizing globally

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component.jsx';
 import CustomButton from '../custom-button/custom-button.component.jsx';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils.js';
+import {signInWithGoogle} from '../../firebase/firebase.utils.js';
 
 import './sign-in.styles.scss';
 
@@ -50,8 +50,12 @@ class SignIn extends React.Component {
             required
             handleChange={this.handleChange}
           />
+          <div className="buttons">
           <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            Sign in with Google
+          </CustomButton>
+        </div>
         </form>
       </div>
     );
