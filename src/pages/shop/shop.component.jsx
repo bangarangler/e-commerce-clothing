7a97b715b26a3fdacb2d5d22e50eccd14d6482b3
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-11 19:30:41 -0400

    Added a map selector...
    and with a little currying we now have a very dynamic route system set
    up

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React from 'react';
+import {Route} from 'react-router-dom';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component.jsx'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component.jsx';
+import CollectionPage from '../collection/collection.component.jsx'
 
-const ShopPage = () => {
+const ShopPage = ({match}) => {
   return (
     <div className="shop-page">
-    <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
-
 
 export default ShopPage;
