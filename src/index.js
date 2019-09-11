bbda65a10a82141faf15286437c3d9d845c88928
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-10 20:35:44 -0400

    Added setCurrentUser Action to app and ...
    dispatched it.
    connecting to it in header

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
+import store from './redux/store.js';
+
 import './index.css';
 import App from './App';
 
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
