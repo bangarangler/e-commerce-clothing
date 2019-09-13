f1e9dba7a6c7c57f29f247bf8aab588cdd609936
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-13 08:32:37 -0400

    now have data from firestore saved in store

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component.jsx';
 import CollectionPage from '../collection/collection.component.jsx';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils.js';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils.js';
+
+import { updateCollections } from '../../redux/shop/shop.actions.js';
 
 class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
     collectionRef.onSnapshot(async snapshot => {
-      convertCollectionsSnapshotToMap(snapshot)
-    })
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap)
+    });
   }
 
   render() {
@@ -30,4 +38,9 @@ class ShopPage extends React.Component {
   }
 }
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
