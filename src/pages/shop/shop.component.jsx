a4a5c1823b488da554fb4f926d13d236c54911c6
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-13 10:27:52 -0400

    Dealt with small bug...
    now collections page refresh works as expected

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,7 +7,7 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection.component.jsx';
 
 import {fetchCollectionsStartAsync} from '../../redux/shop/shop.actions.js';
-import {selectIsCollectionFetching} from '../../redux/shop/shop.selectors.js';
+import {selectIsCollectionFetching, selectIsCollectionsLoaded} from '../../redux/shop/shop.selectors.js';
 import {updateCollections} from '../../redux/shop/shop.actions.js';
 import WithSpinner from '../../components/with-spinner/with-spinner.component.jsx';
 
@@ -21,7 +21,7 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const {match, isCollectionFetching} = this.props;
+    const {match, isCollectionFetching, isCollectionsLoaded} = this.props;
     return (
       <div className="shop-page">
         <Route
@@ -38,7 +38,7 @@ class ShopPage extends React.Component {
           path={`${match.path}/:collectionId`}
           render={props => (
             <CollectionPageWithSpinner
-              isLoading={isCollectionFetching}
+              isLoading={!isCollectionsLoaded}
               {...props}
             />
           )}
@@ -50,6 +50,7 @@ class ShopPage extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   isCollectionFetching: selectIsCollectionFetching,
+  isCollectionsLoaded: selectIsCollectionsLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
